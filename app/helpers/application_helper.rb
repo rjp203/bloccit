784d1e9d2f20880a028de46f1ef50be698438491
Ryan Popovic <rjp203@hotmail.com>
2017-07-14 01:17:20 +0000

    Checkpoint 22 Validations: Completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def form_group_tag(error, &block)
+    css_class = 'form-group'
+    css_class << ' has-error' if error.any?
+    content_tag :div, capture(&block), class: css_class
+  end
 end
