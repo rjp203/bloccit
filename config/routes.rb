b1e8818d2e1c1d7302cac538a75ef4c3d2fba064
Ryan Popovic <rjp203@hotmail.com>
2017-07-15 19:54:34 +0000

    Assignment 24 Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index]
-  end
+  end 
   
   resources :users, only: [:new, :create]
+  post 'users/confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
-
+  
   root 'welcome#index'
 end
