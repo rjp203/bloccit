6425b037e24097999966e6f988611923fea8e21c
Ryan Popovic <rjp203@hotmail.com>
2017-07-17 01:57:13 +0000

    Checkpoint 27 Posts and Users: Completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
   
+  it { is_expected.to have_many(:posts) }
+  
   #Shoulda tests for email
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email) }
