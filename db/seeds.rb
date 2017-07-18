1159d25447dbb58a318af16d0863357b4ac406f9
Ryan Popovic <rjp203@hotmail.com>
2017-07-18 02:00:30 +0000

    Checkpoint 28 Authorization: Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,11 +34,18 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'rjp203@gmail.com',
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
   password: 'helloworld'
-  )
+)
 
 puts "Seed finished"
 puts "#{User.count} users created"
